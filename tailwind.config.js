a5282d27dda18d29be78454ad7d83c36ff7d715c
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2023-07-19 20:29:25 +0100

    Add tailwind variables and tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,43 @@
 
 module.exports = {
   content: [
-    "./evoke-creative/**/*.html"
+    './evoke-creative/**/*.html'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+    },
+    fontFamily: {
+      'sans': 'var(--primary-font-family)',
+      'serif': 'var(--secondary-font-family)'
+    },
+    extend: {
+      colors: {
+        'primary': 'var(--primary-color)',
+        'secondary': 'var(--secondary-color)',
+        'tertiary': 'var(--tertiary-color)',
+        'black': 'var(--black-color)',
+        'dark-grey': 'var(--dark-grey-color)',
+        'light-grey': 'var(--light-grey-color)',
+        'white': 'var(--white-color)'
+      },
+      fontSize: {
+        'x-small': 'var(--body-font-size-small)',
+        'small': 'var(--body-font-size-mobile)',
+        'medium': 'var(--body-font-size)', 
+        'large': 'var(--body-font-size-large)',
+        'x-large': 'var(--body-font-size-x-large)'
+      },
+      borderRadius: {
+        'theme': 'var(--border-radius)'
+      }
+    }
   },
   plugins: []
 }
+
 
